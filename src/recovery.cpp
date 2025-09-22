973c0fa08f987b05af69d04844a6188620accd76
islay <islayross08@gmail.com>
2025-09-22 10:47:51 +0100

    read usb serial output

--- src/recovery.cpp
+++ src/recovery.cpp
@@ -1,10 +1,32 @@
 #include <Arduino.h>
 
+char receivedChar;
+boolean newData = false;
+
+
+void recvOneChar() {
+    if (Serial.available() > 0) {
+        receivedChar = Serial.read();
+        newData = true;
+    }
+}
+
+void showNewData() {
+    if (newData) {
+        Serial.print("This just in ... ");
+        Serial.println(receivedChar);
+        newData = false;
+    }
+}
+
 void recovery()
 {
   // this code runs in recovery mode
-  while (true)
-  {
-    delay(80); // stay here
+  Serial.begin(9600);
+  Serial.println("Entered recovery mode.");
+
+  while(true) {
+    recvOneChar();
+    showNewData();
   }
 }
