6cc6eb3aa0999714e99b03af94318f19819b17f2
islay <islayross08@gmail.com>
2025-09-23 23:10:15 +0100

    Option selection created for recovery mode.

--- src/recovery.cpp
+++ src/recovery.cpp
@@ -5,27 +5,64 @@ bool newData = false;
 
 
 void recvOneChar() {
-    if (Serial.available() > 0) {
-        receivedChar = Serial.read();
-        newData = true; 
-    }
+  if (Serial.available() > 0) {
+    receivedChar = Serial.read(); // reads a single character
+    newData = true; 
+  }
 }
 
 void showNewData() {
-    if (newData) {
-        Serial.print("This just in ... ");
-        Serial.println(receivedChar);
-        newData = false;
-    }
+  if (newData) {
+    Serial.print("Press 't' to set the time, 'f' to read the flash, or 'q' to quit.");
+    newData = false;
+  }
 }
 
-void recovery()
-{
-  // this code runs in recovery mode
-  Serial.println("Entered recovery mode."); 
+int setTimeMode() {
+  Serial.println("Setting time not yet implemented...");
+  return EXIT_SUCCESS;
+}
 
-  while(true) {
+int flashDumpMode() {
+  Serial.println("Flash dump not yet implemented...");
+  return EXIT_SUCCESS;
+}
+
+void recovery() {
+  bool running = true;
+
+  while (running) {
+  Serial.println("Entered recovery mode. \n Press any key to continue."); 
+
+  while (!newData) {
     recvOneChar();
     showNewData();
   }
-}
+
+  char choice = receivedChar;
+  newData = false;
+  int status = EXIT_SUCCESS;
+
+  switch (choice) {
+    case 't':
+    case 'T':
+      status = setTimeMode();
+      break;
+
+    case 'f':
+    case 'F':
+      status = flashDumpMode();
+      break;
+
+    case 'q':
+    case 'Q':
+      running = false;
+      Serial.println("Exiting recovery mode.");
+      break;
+
+    default:
+      Serial.println("Invalid choice.");
+      status = EXIT_FAILURE;
+      break;
+  }
+}}
