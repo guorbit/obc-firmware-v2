b43cff51baeda3519402c09a5cad250400cdc656
ksmoishepang <moishekspang@gmail.com>
2025-10-28 08:18:38 +0000

    some formatting on ADCS function

--- src/adcs.cpp
+++ src/adcs.cpp
@@ -1,6 +1,4 @@
 #include <adcs.h>
-
-
 float readADCS(bool adcsreading, float reading){
     if (adcsreading==true){return reading;} //if a adcs reading is received, then the reading is return from the function.
     else {cout<<"Reading ADCS is not yet implemented"<<endl;} // the case when adcs reading is not received.
