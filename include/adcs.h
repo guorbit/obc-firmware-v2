8314865a08650532d7290208911129cb290f92e6
ksmoishepang <moishekspang@gmail.com>
2025-10-28 08:17:59 +0000

    readADCS function added

---
+++ include/adcs.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool adcsreading; // define whether the adcs reading is received or not
+// if pin number for the adcs system= High, bool adcsreadfing= true.
+
+
