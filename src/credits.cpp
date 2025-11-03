88cb959c57aea38fd40db6f0219950cc725983b7
SalihaA-77 <afridisaliha@gmail.com>
2025-11-03 11:23:10 +0000

    adding credits function for contributors

---
+++ src/credits.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //standard library for terminal input and output
+#include "credits.h" //header file for 'credits' function
+
+void credits() {
+    std::cout << "Contributors:" << std::endl; //list of contributors - our credits
+    std::cout << "Saliha Afridi" << std::endl; 
+}
