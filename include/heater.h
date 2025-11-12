ef65aa3835a4ce8f1092fc0fe0b8a1d89c571055
SalihaA-77 <afridisaliha@gmail.com>
2025-11-12 16:27:21 +0000

    adding the skeleton for setHeater function

---
+++ include/heater.h
@@ -0,0 +1,6 @@
+#ifndef HEATER_H
+#define HEATER_H
+
+int setHeater(); //declaring heater function
+
+#endif
---
+++ src/heater.cpp
@@ -0,0 +1,8 @@
+#include <Arduino.h>
+
+bool state = false; // declaring boolean input value
+int status = EXIT_SUCCESS; //declaring variable for either exit success or failure
+
+int setHeater() {
+    status = EXIT_FAILURE; //returning exit failure (for now)
+}
