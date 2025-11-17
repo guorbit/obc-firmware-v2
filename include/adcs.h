7744cf49b0befa949d54614dfe6bbbff09650d1b
Eric Dawe <eric@redocrehtona.dev>
2025-11-17 16:24:28 +0000

    First version of ADCS I²C read code

--- include/adcs.h
+++ include/adcs.h
@@ -1,8 +1,17 @@
 #ifndef ADCS_H
 #define ADCS_H
-#include <string>
-// the function for reading ADCS.
-std::string readADCS();
-// the function for reading ADCS time. 
-std::string readADCSTime();
+
+// The address of the ADCS board
+#define ADCS_I2C_ADDRESS 0x08
+
+// Set the length of the output from the 
+//  ADCS board, in bytes.  
+#define READOUT_LENGTH_ADCS 32
+
+// Set the I²C options
+#define ADCS_I2C_SDA PB7
+#define ADCS_I2C_SCL PB6
+
+int initADCS();
+int readADCS(char* adcsData);
 #endif
--- src/adcs.cpp
+++ src/adcs.cpp
@@ -1,7 +1,27 @@
-#include <string>
-std::string readADCS() { 
-    return "Reading ADCS is not yet implemented "; 
+#include "adcs.h"
+#include <Wire.h>
+#include <Arduino.h>
+
+TwoWire i2c0 = TwoWire(0);
+
+int initADCS() {
+    i2c0.begin(ADCS_I2C_SDA, ADCS_I2C_SCL, 100000);
+    return EXIT_SUCCESS;
 }
-std::string readADCSTime(){
-    return "2025-11-06T17:30:15Z";
+
+int readADCS(char* adcsData) { 
+    i2c0.requestFrom(ADCS_I2C_ADDRESS, READOUT_LENGTH_ADCS);
+
+    for (int i = 0; i < READOUT_LENGTH_ADCS; i++) {
+        adcsData[i] = i2c0.read();
+    }
+
+    /*
+    while (i2c0.available()) {
+        char c = i2c0.read();
+        Serial.println(c);
+    }
+    */
+
+    return EXIT_SUCCESS;
 }
