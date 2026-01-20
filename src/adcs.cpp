73be6e9fda04bbaee7aa433fde3a10ef82d9e309
Eric Dawe <eric@redocrehtona.dev>
2026-01-20 17:57:10 +0000

    Updated ADCS code to first working prototype

--- src/adcs.cpp
+++ src/adcs.cpp
@@ -2,25 +2,27 @@
 #include <Wire.h>
 #include <Arduino.h>
 
-TwoWire i2c0 = TwoWire(0);
+TwoWire i2c1(ADCS_I2C_SDA, ADCS_I2C_SCL);
 
 int initADCS() {
-    i2c0.begin(ADCS_I2C_SDA, ADCS_I2C_SCL, ADCS_I2C_SPEED);
+    i2c1.setSDA(ADCS_I2C_SDA);
+    i2c1.setSCL(ADCS_I2C_SCL);
+    i2c1.begin(); //, ADCS_I2C_SPEED);
     return EXIT_SUCCESS;
 }
 
 int readADCS(char* adcsData) { 
-    i2c0.requestFrom(ADCS_I2C_ADDRESS, READOUT_LENGTH_ADCS);
+    int bytesRecieved = i2c1.requestFrom(ADCS_I2C_ADDRESS, READOUT_LENGTH_ADCS);
 
     for (int i = 0; i < READOUT_LENGTH_ADCS; i++) {
-        if (i2c0.available()) {
-            adcsData[i] = i2c0.read();
-        }
+        if (i2c1.available()) {
+            adcsData[i] = i2c1.read();
+        } 
     }
 
     /*
-    while (i2c0.available()) {
-        char c = i2c0.read();
+    while (i2c1.available()) {
+        char c = i2c1.read();
         Serial.println(c);
     }
     */
